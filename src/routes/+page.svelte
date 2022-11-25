962f05e38320b335c698215413df5bee2cddc666
Romain Crestey <rcrestey@gmail.com>
2022-11-25 16:14:26 +0100

    chore: add autoprefixer and sass env imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to Annuaire</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="sass">
+  p
+    color: $green
+
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		sass: {
+			prependData: `@import './src/styles/env'`,
+			renderSync: true,
+		},
+		postcss: {
+			plugins: [autoprefixer],
+		},
+	}),
 
 	kit: {
 		adapter: adapter(),
