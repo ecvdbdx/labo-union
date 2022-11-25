0eb2b44de6d585f5ee8af255b30398e15f49f0f2
Romain Crestey <rcrestey@gmail.com>
2022-11-25 12:01:35 +0100

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => require('typescript'),
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+	rules: {
+		eqeqeq: ['error', 'always', { null: 'ignore' }],
+		'no-alert': 'error',
+		'prefer-arrow-callback': 'error',
+		'no-console': ['error', { allow: ['warn', 'error'] }],
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{
+				args: 'after-used',
+				ignoreRestSiblings: true,
+			},
+		],
+	},
+};
