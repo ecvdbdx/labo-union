04c8ce4e386bccda6906f94fbf8fb793af36ab00
Valentin Magry <valou32720@gmail.com>
2022-12-12 13:19:18 +0100

    feat: create Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let type: 'default' | 'rounded' | 'square' = 'default';
+</script>
+
+<button class="btn {type}" on:click>
+	<slot>Text Label</slot>
+</button>
+
+<style lang="scss">
+	@use './src/lib/styles/colors';
+
+	.btn {
+		color: colors.$white;
+		background-color: colors.$primary;
+		padding: 0.6rem 1rem;
+		border-radius: 10px;
+		border: none;
+		cursor: pointer;
+		transition: 0.35s ease;
+
+		&:disabled {
+			background-color: colors.$disabled;
+			cursor: not-allowed;
+		}
+
+		&:hover {
+			background-color: colors.$hover;
+		}
+
+		&:focus {
+			background-color: colors.$focus;
+		}
+
+		&:active {
+			background-color: colors.$active;
+		}
+
+		&.rounded {
+			border-radius: 50px;
+		}
+
+		&.square {
+			border-radius: 0;
+		}
+	}
+</style>
