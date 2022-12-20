53f712a0c105a24fd19ceaf85b7b0b8672bca443
Valentin Magry <valou32720@gmail.com>
2022-12-20 16:09:06 +0100

    feat: handle form error and email / password validation

--- src/routes/inscription/+page.server.ts
+++ src/routes/inscription/+page.server.ts
@@ -1,24 +1,51 @@
 import type { Actions } from './$types';
+import { fail } from '@sveltejs/kit';
 
 import { supabase } from '$lib/auth';
 
 export const actions: Actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
-		const email = formData.get('email');
-		const password = formData.get('password');
-		const termAndConditions = formData.get('termAndConditions');
 
-		if (!email) {
-			return { status: 400, email: 'Veuillez renseigner votre adresse email' };
+		const email = formData.get('email')?.toString();
+		const password = formData.get('password')?.toString();
+		const termAndConditions = formData.get('termAndConditions')?.toString();
+
+		if (!email || !password || !termAndConditions) {
+			return fail(400, {
+				email_error: !email ? 'Veuillez renseigner votre adresse email' : null,
+				password_error: !password ? 'Veuillez renseigner votre mot de passe' : null,
+				termAndConditions_error: !termAndConditions
+					? "Veuillez accepter les conditions d'utilisation"
+					: null,
+				email,
+				password,
+			});
 		}
 
-		if (!password) {
-			return { status: 400, password: 'Veuillez renseigner votre mot de passe' };
+		if (password.toString().length < 9) {
+			return fail(400, {
+				password_error: 'Votre mot de passe doit faire au moins 9 caractères',
+				email,
+				password,
+			});
 		}
 
-		if (!termAndConditions) {
-			return { status: 400, termAndConditions: "Veuillez accepter les conditions d'utilisation" };
+		if (!isValidPassword(password)) {
+			return fail(400, {
+				password_error:
+					'Votre mot de passe doit contenir au moins 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial',
+				email,
+				password,
+			});
+		}
+
+		if (!isEmailValid(email)) {
+			return fail(400, {
+				email_error: "Vous devez utiliser une adresse mail de l'école",
+				email,
+				password,
+			});
 		}
 
 		await supabase.auth.signUp({
@@ -27,3 +54,13 @@ export const actions: Actions = {
 		});
 	},
 };
+
+function isValidPassword(password: string): boolean {
+	const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,}$/;
+	return regex.test(password);
+}
+
+function isEmailValid(email: string): boolean {
+	const emailRegex = /^[^@]+@mail-ecv\.fr$/;
+	return emailRegex.test(email);
+}
