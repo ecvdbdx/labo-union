e56ccdbc434dbf0f7cfc43c31b96e10a66c66e8a
Valentin Magry <valou32720@gmail.com>
2022-12-20 16:09:06 +0100

    feat: handle form error and email / password validation

--- src/routes/inscription/+page.svelte
+++ src/routes/inscription/+page.svelte
@@ -2,35 +2,39 @@
 	import type { ActionData } from './$types';
 
 	import { enhance } from '$app/forms';
+
 	import Input from '$lib/components/forms/Input.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import Checkbox from '$lib/components/forms/Checkbox.svelte';
 	import Link from '$lib/components/Link.svelte';
 
 	export let form: ActionData;
-
-	$: emailError = form?.email;
-	$: passwordError = form?.password;
-	$: termAndConditionsError = form?.termAndConditions;
 </script>
 
 <div class="container sign-up">
 	<Link href="/">Retour</Link>
 
 	<form method="POST" use:enhance>
-		<Input value="" name="email" type="email" placeholder="john.doe@mail-ecv.com" error={emailError}
-			>Email</Input
+		<Input
+			value={form?.email ?? ''}
+			name="email"
+			type="email"
+			placeholder="john.doe@mail-ecv.com"
+			error={form?.email_error}>Email</Input
 		>
-		<Input value="" name="password" type="password" error={passwordError}>Mot de passe</Input>
+		<Input value={form?.password ?? ''} name="password" type="password" error={form?.password_error}
+			>Mot de passe</Input
+		>
 		<Checkbox
 			name="termAndConditions"
 			reverse
 			label="J’accepte les Conditions d’utilisation et la Politique de confidentialité"
 			value={false}
 		/>
-		{#if form?.termAndConditions}
-			<p class="error">{termAndConditionsError}</p>
+		{#if form?.termAndConditions_error}
+			<p class="error">{form?.termAndConditions_error}</p>
 		{/if}
+
 		<Button>Créer un compte</Button>
 	</form>
 </div>
