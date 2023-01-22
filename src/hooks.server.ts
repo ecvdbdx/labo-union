6066c4395f94cc8678c7db46f646d9a1c1b6e841
marie <marie_maunoury@hotmail.fr>
2023-01-22 22:03:18 +0100

    Try to check the email/password

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,2 @@
 import '$lib/auth';
+import '$lib/supabaseClient'
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,24 +2,42 @@
 	import Input from '$lib/components/forms/Input.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import Link from '$lib/components/Link.svelte';
+
+	import { supabase } from '$lib/supabaseClient';
+
+	let loading = false;
+	let email: string;
+	let password: string;
+
+	const handleLogin = async () => {
+		try {
+			loading = true;
+			const { error } = await supabase.auth.signInWithPassword({
+				email: 'example@email.com',
+				password: 'example-password',
+			});
+			console.log('TRY');
+			if (error) throw error;
+			alert('Check your email for the login link!');
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+			console.log('FINALLY' + email);
+		}
+	};
 </script>
 
 <div class="container">
 	<Link href="/">Retour</Link>
 
-	<form method="POST">
+	<form method="POST" on:submit|preventDefault={handleLogin}>
 		<div class="form-widget">
 			<h1 class="header">Me connecter</h1>
-			<Input
-				value={''}
-				name="email"
-				type="email"
-				required
-				placeholder="john.doe@mail-ecv.com"
-				error={''}>Email</Input
-			>
-			<Input value={''} name="password" required type="password" error={'error'}>Mot de passe</Input
-			>
+			<Input name="Email" type="email" placeholder="Your email" bind:value={email}>Email</Input>
+			<Input bind:value={password} name="password" type="password" required>Mot de passe</Input>
 
 			<Button>Me connecter</Button>
 		</div>
