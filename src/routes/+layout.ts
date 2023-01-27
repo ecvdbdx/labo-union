99f46ee006c7a7d5ea7f526a47c66a5406053453
Valentin Magry <valou32720@gmail.com>
2023-01-27 17:21:24 +0100

    clean: clean unused comments

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,15 +1,7 @@
 import type { LayoutLoad } from './$types';
-import type { LayoutServerLoad } from './$types'
-import { getServerSession } from '@supabase/auth-helpers-sveltekit'
 import { getSupabase } from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutLoad = async (event) => {
 	const { session } = await getSupabase(event);
 	return { session };
 };
-
-// export const loadBis: LayoutServerLoad = async (event) => {
-// 	return {
-// 		session: await getServerSession(event),
-// 	}
-// }
