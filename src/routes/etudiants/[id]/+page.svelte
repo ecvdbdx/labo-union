bde08b718c3faffa01e33f7b13ccfc355904c1bf
Matthieu Dejean <matthieudejean2@gmail.com>
2023-01-27 15:34:19 +0100

    style: complete css and change data profile name

--- src/routes/etudiants/[id]/+page.svelte
+++ src/routes/etudiants/[id]/+page.svelte
@@ -2,11 +2,10 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	$: ({ first_name, last_name, cursus, description, status } = data.profile);
+	$: ({ first_name, last_name, speciality, description, status, grade } = data.profile);
 </script>
 
 <div class="container-page">
-	<!-- <div class="background-container" /> -->
 	<div class="container-top">
 		<div class="left">
 			<img class="img-profile" src="https://picsum.photos/200/300" alt="profil" />
@@ -17,8 +16,9 @@
 				{status ? 'Je suis disponible' : 'Je suis pas disponible'}
 				<div class={status ? 'isavailable' : 'notavailable'} />
 			</div>
-			<div class="cursus">
-				{cursus}
+			<div class="grade">
+				{grade}
+				{speciality}
 			</div>
 			<div class="presentation">
 				<p>
@@ -34,46 +34,49 @@
 
 	<div class="container-bottom">
 		<div class="professional-container">
-			<h2 class="degree-container-title">Expériences Professionnelles</h2>
-			<div class="duration-experience">
-				<p>2017-2022</p>
-			</div>
-			<div class="name-experience">
-				<p>UI designer</p>
-			</div>
+			<h2 class="experience-container-title">Expériences Professionnelles</h2>
+			<div class="block-experience">
+				<div class="duration-experience">
+					<p>2017-2022</p>
+				</div>
+				<div class="name-experience">
+					<p>UI designer</p>
+				</div>
 
-			<div class="name-enterprise">
-				<p>Back Market</p>
-			</div>
+				<div class="name-enterprise">
+					<p>Back Market</p>
+				</div>
 
-			<div class="location-experience">
-				<p>Bordeaux, France</p>
-			</div>
+				<div class="location-experience">
+					<p>Bordeaux, France</p>
+				</div>
 
-			<div class="actions-experience">
-				<h4>Actions confiées</h4>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus dignissim dolor at
-					lacinia. Suspendisse cursus mollis dolor eu mattis.
-				</p>
+				<div class="actions-experience">
+					<h4>Actions confiées</h4>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus dignissim dolor at
+						lacinia. Suspendisse cursus mollis dolor eu mattis.
+					</p>
+				</div>
 			</div>
 		</div>
 		<div class="degree-container">
 			<h2 class="degree-container-title">Formations</h2>
+			<div class="block-degree">
+				<div class="duration-degree">
+					<p>2022</p>
+				</div>
+				<div class="name-school">
+					<p>ECV Digital</p>
+				</div>
 
-			<div class="duration-degree">
-				<p>2022</p>
-			</div>
-			<div class="name-school">
-				<p>ECV Digital</p>
-			</div>
+				<div class="location-degree">
+					<p>Paris, Île-de-France, FR</p>
+				</div>
 
-			<div class="location-degree">
-				<p>Paris, Île-de-France, FR</p>
-			</div>
-
-			<div class="name-degree">
-				<p>Bachelor 3 Digital Project Manager, UX Design speciality, Honors</p>
+				<div class="name-degree">
+					<p>Bachelor 3 Digital Project Manager, UX Design speciality, Honors</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -128,7 +131,7 @@
   border-radius: 50%
   margin-left: 0.313rem
 
-.cursus
+.grade
   margin: 0.75rem 0
   font-weight: 600
   color: #18181860
@@ -150,32 +153,44 @@
   box-shadow: 50px -20px #B6AEA7
 
 .container-bottom
+  margin: 7.5rem
   display: flex
   flex-direction: row
-  justify-content: space-around
+  flex-wrap: wrap
+  justify-content: space-between
 
 .professional-container
+  max-width: 40vw
   background-color: #FFFCFA
   border-radius: 30px
   padding: 16px 24px
+  margin-bottom: 15px
 
 .degree-container-title
   color: #F37144
 
+.experience-container-title
+  color: #F37144
+
 .degree-container
+  max-width: 40vw
   background-color: #FFFCFA
   border-radius: 30px
   padding: 16px 24px
+  margin-bottom: 15px
 
 .name-enterprise
   color: #F25824
   text-decoration: underline
+  grid-column: 2
 
 .location-experience
   color: #18181860
+  grid-column: 2
 
 .actions-experience
   color: #18181860
+  grid-column: 2
 
 .location-degree
   color: #18181860 
@@ -183,4 +198,32 @@
 .name-degree
   color: #18181860
 
+.block-experience
+  display: grid
+  grid-template-columns: repeat(1fr, 2fr)
+  grid-gap: 20px
+
+  
+.duration-experience
+  grid-column: 1
+  
+.name-experience
+  grid-column: 2
+
+.block-degree
+  display: grid
+  grid-template-columns: repeat(1fr, 2fr)
+  grid-gap: 20px
+
+.duration-degree
+  grid-column: 1
+
+.name-school
+  grid-column: 2
+
+.name-degree
+  grid-column: 2
+
+.location-degree
+  grid-column: 2
 </style>
