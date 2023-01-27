c0392ed0b0726cb17f8ba47bde32a0718948a3f9
Valentin Magry <valou32720@gmail.com>
2023-01-27 15:40:22 +0100

    feat: remove usless supabase client

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import Link from '$lib/components/Link.svelte';
-	import Icon from '$lib/components/Icon.svelte';
 </script>
 
 <div class="container">
 	<h1>Welcome to Annuaire</h1>
-	<p>
-		Visit <Link href="https://kit.svelte.dev">kit.svelte.dev</Link> to read the documentation
-		<Icon id="bookmark" />
-	</p>
 
 	<Link href="inscription">S'enregistrer</Link>
 </div>
