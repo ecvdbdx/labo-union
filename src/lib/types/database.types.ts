36c6599414a0cf08f038c17941d30879b454f1ec
Florian <mrvdpflorian@gmail.com>
2023-02-10 15:33:32 +0100

    feat: start update exp

--- src/lib/types/database.types.ts
+++ src/lib/types/database.types.ts
@@ -1,4 +1,5 @@
 export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
+
 export interface Database {
 	public: {
 		Tables: {
@@ -37,8 +38,41 @@ export interface Database {
 					speciality?: string | null;
 				};
 			};
+			Experience: {
+				Row: {
+					company: string | null;
+					created_at: string | null;
+					end_date: string | null;
+					id: number;
+					job: string | null;
+					mission: string | null;
+					profile_id: number | null;
+					start_date: string | null;
+				};
+				Insert: {
+					company?: string | null;
+					created_at?: string | null;
+					end_date?: string | null;
+					id?: number;
+					job?: string | null;
+					mission?: string | null;
+					profile_id?: number | null;
+					start_date?: string | null;
+				};
+				Update: {
+					company?: string | null;
+					created_at?: string | null;
+					end_date?: string | null;
+					id?: number;
+					job?: string | null;
+					mission?: string | null;
+					profile_id?: number | null;
+					start_date?: string | null;
+				};
+			};
 			Profile: {
 				Row: {
+					cover_img: string | null;
 					created_at: string | null;
 					date_of_birth: string | null;
 					description: string | null;
@@ -46,49 +80,105 @@ export interface Database {
 					grade: string | null;
 					id: number;
 					last_name: string | null;
+					profile_img: string | null;
 					speciality: string | null;
 					status: boolean | null;
 					user_id: string | null;
-					grade: string | null;
-					speciality: string | null;
+				};
+				Insert: {
+					cover_img?: string | null;
+					created_at?: string | null;
+					date_of_birth?: string | null;
+					description?: string | null;
+					first_name?: string | null;
+					grade?: string | null;
+					id?: number;
+					last_name?: string | null;
+					profile_img?: string | null;
+					speciality?: string | null;
+					status?: boolean | null;
+					user_id?: string | null;
+				};
+				Update: {
+					cover_img?: string | null;
+					created_at?: string | null;
+					date_of_birth?: string | null;
+					description?: string | null;
+					first_name?: string | null;
+					grade?: string | null;
+					id?: number;
+					last_name?: string | null;
+					profile_img?: string | null;
+					speciality?: string | null;
+					status?: boolean | null;
+					user_id?: string | null;
+				};
+			};
+			Training: {
+				Row: {
+					created_at: string | null;
+					diploma: string | null;
+					end_date: string | null;
+					id: number;
+					location: string | null;
+					profile_id: number | null;
+					school: string | null;
+					start_date: string | null;
 				};
 				Insert: {
 					created_at?: string | null;
-					date_of_birth?: string | null;
-					description?: string | null;
-					first_name?: string | null;
-					grade?: string | null;
+					diploma?: string | null;
+					end_date?: string | null;
 					id?: number;
-					last_name?: string | null;
-					speciality?: string | null;
-					status?: boolean | null;
-					user_id?: string | null;
-					grade?: string | null;
-					speciality?: string | null;
+					location?: string | null;
+					profile_id?: number | null;
+					school?: string | null;
+					start_date?: string | null;
 				};
 				Update: {
 					created_at?: string | null;
-					date_of_birth?: string | null;
-					description?: string | null;
-					first_name?: string | null;
-					grade?: string | null;
+					diploma?: string | null;
+					end_date?: string | null;
 					id?: number;
-					last_name?: string | null;
-					speciality?: string | null;
-					status?: boolean | null;
-					user_id?: string | null;
-					grade?: string | null;
-					speciality?: string | null;
+					location?: string | null;
+					profile_id?: number | null;
+					school?: string | null;
+					start_date?: string | null;
 				};
 			};
 		};
 		Views: {
-			[_ in never]: never;
+			distinct_grade: {
+				Row: {
+					grade: string | null;
+				};
+			};
+			distinct_speciality: {
+				Row: {
+					speciality: string | null;
+				};
+			};
 		};
 		Functions: {
-			[_ in never]: never;
+			searchprofile:
+				| {
+						Args: {
+							table_name: string;
+							search: string;
+						};
+						Returns: undefined;
+				  }
+				| {
+						Args: {
+							search: string;
+						};
+						Returns: undefined;
+				  };
 		};
 		Enums: {
+			[_ in never]: never;
+		};
+		CompositeTypes: {
 			[_ in never]: never;
 		};
 	};
