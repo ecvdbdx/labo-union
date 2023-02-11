f47152594ade509680408db6a7d3933be36487b9
Valentin Magry <valou32720@gmail.com>
2023-02-11 19:00:09 +0100

    feat: get user_id from supabase session

--- src/routes/profil/+page.ts
+++ src/routes/profil/+page.ts
@@ -1,14 +1,17 @@
+import type { PageServerLoad } from './$types';
+
 import { error } from '@sveltejs/kit';
 import { supabase } from '$lib/auth';
-import { fakeAccount_user_id } from '$lib/constants';
 import type { User } from '@interfaces';
 
-export async function load() {
+export const load: PageServerLoad = async ({ parent }) => {
+	const { session } = await parent();
+	const user_id = session?.user.id;
+
 	const { data, error: err } = await supabase
 		.from('Profile')
 		.select()
-		//get accountuserId
-		.eq('user_id', fakeAccount_user_id)
+		.eq('user_id', user_id)
 		.single();
 
 	if (err) {
@@ -20,4 +23,4 @@ export async function load() {
 	return {
 		profile: data as User,
 	};
-}
+};
