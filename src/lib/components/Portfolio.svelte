2ff10d82a1a3150a40e87c131aa78a2e6761e1f5
Matthieu Dejean <matthieudejean2@gmail.com>
2023-03-03 17:52:15 +0100

    feat: portfolio feature done

---
+++ src/lib/components/Portfolio.svelte
@@ -0,0 +1,53 @@
+<script>
+	const portfolioData = [
+		{
+			image: 'https://picsum.photos/id/5/600/400',
+			img_alt: 'application',
+			title: 'Application',
+			description: 'Product Design',
+		},
+		{
+			image: 'https://picsum.photos/id/6/600/400',
+			img_alt: 'site web',
+			title: 'Site Web',
+			description: 'E-commerce',
+		},
+		{
+			image: 'https://picsum.photos/id/7/600/400',
+			img_alt: 'maquette',
+			title: 'Maquette UX/UI',
+			description: 'Design',
+		},
+	];
+</script>
+
+<div class="grid-container">
+	{#each portfolioData as portfolioItem}
+		<div>
+			<img src={portfolioItem.image} alt={portfolioItem.img_alt} />
+			<h3>{portfolioItem.title}</h3>
+			<p>{portfolioItem.description}</p>
+		</div>
+	{/each}
+</div>
+
+<style lang="sass">
+.grid-container
+  display: grid
+  grid-template-columns: repeat(3, 1fr)
+  grid-gap: 2rem
+
+h3
+  padding-top: 0.625rem
+
+img
+  object-fit: cover
+  width: 100%
+  height: auto
+  border-radius: 1.875rem
+
+p
+  color: #18181899
+  padding-top: 0.625rem
+
+</style>
