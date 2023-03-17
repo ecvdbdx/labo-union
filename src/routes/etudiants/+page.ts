d110930b4e628063a34fa23aded71b8c2e50533c
Keshia <72273200+keshiamukenge@users.noreply.github.com>
2023-03-17 11:57:40 +0100

    Feat/search student (#26)
    * feat: add search on profiles list
    
    * fix: dynamic style & clean responsive and style
    
    ---------
    
    Co-authored-by: Keshia Mukenge <keshia@MacBook-Pro-de-Keshia.local>
    Co-authored-by: Keshia Mukenge <keshia@MBP-de-Keshia.lan>

--- src/routes/etudiants/+page.ts
+++ src/routes/etudiants/+page.ts
@@ -3,16 +3,29 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-	const { data: profiles, error: err } = await supabase.from('Profile').select('*');
+	const { data: profiles, error: profilesErr } = await supabase.from('Profile').select('*');
+	const { data: grade, error: gradeErr } = await supabase.from('distinct_grade').select();
+	const { data: speciality, error: specialityErr } = await supabase
+		.from('distinct_speciality')
+		.select();
 
-	if (err) {
+	if (profilesErr || gradeErr || specialityErr) {
 		throw error(500, {
 			code: 500,
 			message: 'Une erreur est survenue',
 		});
 	}
 
+	if (!profiles || profiles === null) {
+		throw error(404, {
+			code: 404,
+			message: 'Aucun profil trouvé',
+		});
+	}
+
 	return {
 		profiles,
+		grade,
+		speciality,
 	};
 }) satisfies PageLoad;
---
+++ src/routes/etudiants/Filter.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let options: Record<string, string>[];
+	export let label: string;
+
+	let selectedOption: string;
+	let key = Object.keys(options[0])[0];
+</script>
+
+<div class="Filter">
+	<label for="data-id">{label}</label>
+	<select bind:value={selectedOption} on:change>
+		<option value="">Sélectionner</option>
+		{#each options as option}
+			<option value={option[key]}>{option[key]}</option>
+		{/each}
+	</select>
+</div>
+
+<style lang="sass">
+.Filter
+	display: flex
+	flex-direction: column
+	width: 11rem
+	margin-right: 3.4rem
+
+label
+	margin: 0.3rem
+	font-size: 0.75rem
+	font-weight: 500
+</style>
