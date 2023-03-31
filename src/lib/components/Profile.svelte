0e55a59caaa6f9b3cab08eb3c27fb45d68186393
TontonduWebEcv <115226547+TontonduWebEcv@users.noreply.github.com>
2023-03-31 17:55:12 +0200

    #47 Remove image from bucket when deleting or replacing (#57)
    * #47 Remove image from bucket when deleting or replacing
    
    * refactor: previous commit
    
    ---------
    
    Co-authored-by: matthieu-scub <matthieu.dejean@scub.net>

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -65,12 +65,19 @@
 	}
 
 	async function deleteImg() {
+		const currentImg = profile.profile_img.split('image-profile/')[1];
 		const { error: err } = await supabase
 			.from('Profile')
 			.update({ profile_img: '' })
 			.eq('user_id', userId);
+		const { error: errRemoveImg } = await supabase.storage
+			.from('image-profile')
+			.remove([currentImg]);
 		if (err) {
 			return err;
+		}
+		if (errRemoveImg) {
+			return errRemoveImg;
 		}
 
 		invalidate('app:profile');
