21646ae6570de3d4940ed1324195500d8acd65a5
Keshia <72273200+keshiamukenge@users.noreply.github.com>
2023-03-31 16:30:08 +0200

    fix: limit uploaded img weight (#51)
    * fix: limit uploaded img weight
    
    * fix: add const for size limit
    
    ---------
    
    Co-authored-by: Keshia Mukenge <keshia@MacBook-Pro-de-Keshia.local>

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -26,8 +26,11 @@
 
 	let displayEditSummary = false;
 	let displayEditProfilImg = false;
+	$: imageIsTooBig = false;
 
 	let isCurriculum = true;
+
+	const sizeLimitInKo = 500;
 
 	const handleOpenEditModal = () => (displayEditSummary = true);
 	const handleOpenImgModal = () => (displayEditProfilImg = true);
@@ -44,15 +47,21 @@
 		}
 
 		const file = files[0];
-		const format = file.name.split('.').pop();
 
-		profile.profile_img = URL.createObjectURL(file);
+		if (file.size <= sizeLimitInKo * 1000) {
+			imageIsTooBig = false;
+			const format = file.name.split('.').pop();
 
-		const hashProfile = uuidv4();
+			profile.profile_img = URL.createObjectURL(file);
 
-		const filePath = `${hashProfile}-profile.${format}`;
+			const hashProfile = uuidv4();
 
-		uploadImg(userId, filePath, currentImg, file);
+			const filePath = `${hashProfile}-profile.${format}`;
+
+			uploadImg(userId, filePath, currentImg, file);
+		} else {
+			imageIsTooBig = true;
+		}
 	}
 
 	async function deleteImg() {
@@ -231,6 +240,9 @@
 				</div>
 			{/if}
 		</div>
+		{#if imageIsTooBig}
+			<p class="big-img-msg">Votre image dépasse {sizeLimitInKo} ko.</p>
+		{/if}
 		<div class="modifyImg">
 			<div class="params">
 				<label class="button primary block" for="single">
@@ -519,6 +531,11 @@ h2
   border: none
   background-color: transparent
   cursor: pointer
+
+.big-img-msg
+  text-align: center
+  margin-top: 1rem
+  font-size: 0.8rem
 
 .form 
   display: flex
