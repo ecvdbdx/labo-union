62e8167af359de46eadcfb126a45e4fabd47352c
Valentin Magry <valou32720@gmail.com>
2023-04-20 11:31:14 +0200

    fix: wrong vercel url env variable

--- src/lib/utils/getUrl.ts
+++ src/lib/utils/getUrl.ts
@@ -1,6 +1,6 @@
 export const getURL = () => {
 	let url =
-		process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+		process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
 		'http://localhost:8080/';
 	// Make sure to include `https://` when not localhost.
 	url = url.includes('http') ? url : `https://${url}`;
